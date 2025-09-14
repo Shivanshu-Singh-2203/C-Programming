a0ce753eed1b08a65404401525b213c16b0dba65
Shivanshu Singh <shivanshusingh2203@gmail.com>
2025-09-14 21:01:54 +0530

    Functions

--- Functions/isPrime.c
+++ Functions/isPrime.c
@@ -6,15 +6,14 @@
 bool isPrime(int n)
 {
     if (n <= 1) 
-        return false;   // 0 and 1 are not prime
+        return false;  
     
     for (int i = 2; i * i <= n; i++)
     {
         if (n % i == 0)
-            return false;   // divisible → not prime
+            return false; 
     }
-    return true; // if no divisor found, it's prime
-}
+    return true;
 
 int main(void)
 {
