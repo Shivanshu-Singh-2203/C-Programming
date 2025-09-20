4c0f1f4c4f06d0d4c9539cac29368d0c07124ebb
Shivanshu-Singh-2203 <shivanshusingh2203@gmail.com>
2025-09-20 10:37:37 +0530

    Removed Multifile program folder

--- Multifile Program/Matrix/matrix.h
+++
@@ -1,9 +0,0 @@
-#ifndef MATRIX_H
-#define MATRIX_H
-
-void add_matrix(int a[2][2], int b[2][2], int result[2][2], int rows, int cols);
-void multiply_matrix(int a[2][2], int b[2][2], int result[2][2], int rows, int cols, int common);
-void print_matrix(int m[2][2], int rows, int cols);
-
-#endif
-
