8bf7db63c77f205766ec43156f49726809f34ef5
Shivanshu Singh <shivanshusingh2203@gmail.com>
2025-10-10 02:24:29 +0530

    Stack Implementaion

---
+++ Stacks/Implementation/stack.h
@@ -0,0 +1,16 @@
+#ifndef STACK_H /* defining macros */
+#define STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 100
+
+int push(int);
+int pop(int*);
+int display();
+int peek(int*);
+        
+#endif // !STACK_H
+
+
